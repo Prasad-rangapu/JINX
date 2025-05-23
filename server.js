4385678e9a673fe7864d096d661695029b280bbc
Prasad-rangapu <164445957+Prasad-rangapu@users.noreply.github.com>
2025-05-23 15:57:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => next(createError(404)));
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message });
 });
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
